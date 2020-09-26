aa0f7812dea27bf58676b391a85b2c4e7779b313
Gururaj Sridhar <gsridhar@duosecurity.com>
2020-09-25 19:29:38 -0500

    bootstrap project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+    $('#reserveButton').click(() => {
+        $('#reserveform').modal('show');
+    });
+    $('#loginButton').click(() => {
+        $('#loginModal').modal('show');
+    });
+});
